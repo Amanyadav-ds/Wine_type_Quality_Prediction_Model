3c1989cef188fb4d7f4fac28e812dce6f8bb67f2
Aman Yadav <aman.yadav.analytics@gmail.com>
2026-08-20 11:19:14 +0530

    Fix button color in wine type prediction model

--- wine_type_prediction_model_deployment.py
+++ wine_type_prediction_model_deployment.py
@@ -43,7 +43,7 @@ df=pd.DataFrame({
 
 })
 
-if st.button("Predict Wine Type ",color="blue"):
+if st.button("Predict Wine Type "):
   prediction=model.predict(df)[0]
   if prediction==0:
     st.write("Red wine")
